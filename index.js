feaf0882e7a725480820638dbdbd6ab8963b41d7
David Thai <david.thai@dafuloth.me>
2018-08-26 20:15:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // write your code below!
+var name = "you"
+
 function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(string) {
-  return "Happy holidays, $(string)!"
+function happyHolidaysTo(name) {
+  return "Happy holidays, $(name)!"
 }
 
 function happyHolidayTo(holiday, name) {
