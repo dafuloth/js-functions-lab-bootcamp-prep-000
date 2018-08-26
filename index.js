bfc39681272ef8047b4a6869daab074b26ad78fb
David Thai <david.thai@dafuloth.me>
2018-08-26 20:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(string) {
-  return "Happy holidays, $(string)!"
+function happyHolidaysTo(name) {
+  return "Happy holidays, $(name)!"
+}
+
+function happyHolidayTo(holiday, name) {
+  return "Happy $(holiday), $(name)!"
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's $(days) days until $(holiday)!"
 }
